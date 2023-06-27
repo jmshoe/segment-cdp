02fa2d342ac0b7bcdb004872f406a2d9b5909f30
Jason Macht <jmshoe@gmail.com>
2023-06-27 10:16:35 -0500

    Create mailgun-clicked-custom-source-function.js

---
+++ mailgun-clicked-custom-source-function.js
@@ -0,0 +1,23 @@
+// Learn more about source functions API at
+// https://segment.com/docs/connections/sources/source-functions
+
+/**
+ * Handle incoming HTTP request
+ *
+ * @param  {FunctionRequest} request
+ * @param  {FunctionSettings} settings
+ */
+async function onRequest(request, settings) {
+	const body = request.json();
+	const {
+		'event-data': { recipient, event, tags }
+	} = body;
+	const uuid = crypto.randomUUID();
+	const timestamp = new Date();
+	const ISOString = timestamp.toISOString();
+	Segment.identify({
+		anonymousId: uuid,
+		traits: {
+			email: recipient
+		}
+	});
