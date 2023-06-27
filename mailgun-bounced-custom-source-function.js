6510d86b6099fe679f9dd128fba01fa1977b26b5
Jason Macht <jmshoe@gmail.com>
2023-06-27 10:17:15 -0500

    Create mailgun-bounced-custom-source-function.js

---
+++ mailgun-bounced-custom-source-function.js
@@ -0,0 +1,43 @@
+// Learn more about source functions API at
+// https://segment.com/docs/connections/sources/source-functions
+
+/**
+ * Handle incoming HTTP request
+ *
+ * @param  {FunctionRequest} request
+ * @param  {FunctionSettings} settings
+ */
+async function onRequest(request, settings) {
+	const body = request.json();
+	const {
+		'event-data': { recipient, event, tags, message, storage, severity }
+	} = body;
+	const { url } = storage;
+	const {
+		headers: { subject, from }
+	} = message;
+	const uuid = crypto.randomUUID();
+	const timestamp = new Date();
+	const ISOString = timestamp.toISOString();
+	Segment.identify({
+		anonymousId: uuid,
+		traits: {
+			email: recipient
+		}
+	});
+
+	Segment.track({
+		anonymousId: uuid,
+		event: 'Email Bounced',
+		timestamp: ISOString,
+		properties: {
+			emailSubject: subject,
+			from,
+			event: event,
+			emailStorageURL: url,
+			tags: tags,
+			emailPlatform: 'mailgun',
+			bounceType: severity
+		}
+	});
+}
